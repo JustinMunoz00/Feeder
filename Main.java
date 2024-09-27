e01d8515ce3895526e97b96b998406229580081f
JustinMunoz00 <justinm880@nycstudents.net>
2024-09-27 15:42:55 +0000

    Added To test cases 4,5, and 6

--- Main.java
+++ Main.java
@@ -17,12 +17,15 @@ public class Main
         //Test Case 4
         Feeder f = new Feeder(2400);
         System.out.println(f.simulateManyDays(10, 4));
+        System.out.println(f);
         //Test Case 5
         Feeder k = new Feeder(250);
         System.out.println(k.simulateManyDays(10, 5));
+        System.out.println(k);
         //Test Case 6
         Feeder g = new Feeder(0);
         System.out.println(g.simulateManyDays(5, 10));
+        System.out.println(g);
     }
 
 }
